6c24c0418866690692f090f00754d706b62acf30
Thiago Dias <tdtadeu@gmail.com>
2014-09-19 19:24:26 -0300

    Install Vundle after everything is done

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,4 +12,34 @@ node['vim']['users'].each do |user|
     source "#{node['vim']['gist']}"
     not_if { File.exists?("/home/#{user}/.vimrc") }
   end
+
+  directory "/home/#{user}/.vim" do
+    owner user
+    group user
+  end
+
+  directory "/home/#{user}/.vim/bundle" do
+    owner user
+    group user
+  end
+
+  git "/home/#{user}/.vim/bundle/vim-colors-solarized" do
+    repository "git://github.com/altercation/vim-colors-solarized"
+    action :sync
+    user user
+  end
+
+  execute "run-vundle-#{user}" do
+    action :nothing
+    command "vim +PluginInstall +qall"
+    timeout 600
+    user user
+  end
+
+  git "/home/#{user}/.vim/bundle/Vundle.vim" do
+    repository "git://github.com/gmarik/Vundle.vim.git"
+    action :sync
+    notifies :run, "execute[run-vundle-#{user}]"
+    user user
+  end
 end
