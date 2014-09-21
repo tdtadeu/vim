80cb01d8e817f17c227942e49a5b9d416b43ecaf
Thiago Dias <tdtadeu@gmail.com>
2014-09-21 14:09:13 -0300

    Testing execute with group defined

--- recipes/default.rb
+++ recipes/default.rb
@@ -32,7 +32,8 @@ node['vim']['users'].each do |user|
   execute "run-vundle-#{user}" do
     action :nothing
     command "vim -c 'set shortmess=at' +BundleInstall +qall"
-    # timeout node['vim']['timeout']
+    timeout node['vim']['timeout']
+    group user
     user user
   end
 
